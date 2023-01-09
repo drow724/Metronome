3d33d88d0438eaec3c1670837df95fa7731d7658
drow724 <drow724@naver.com>
2023-01-09 14:22:46 +0900

    test

--- src/TimerUtil.js
+++ src/TimerUtil.js
@@ -1,11 +1,5 @@
-export function loop(setTimeOutTimer, rhythm, bpm) {
-  setTimeOutTimer([]);
-  rhythm.forEach((sound, i) => {
-    setTimeOutTimer((current) => [
-      ...current,
-      setTimeout(() => {
-        sound();
-      }, (60 / bpm) * 1000 * (i + 1)),
-    ]);
-  });
+export default function Webworker(worker) {
+  const code = worker.toString();
+  const blob = new Blob(["(" + code + ")()"]);
+  return new Worker(URL.createObjectURL(blob));
 }
--- src/routes/Webtronome.js
+++ src/routes/Webtronome.js
@@ -1,15 +1,26 @@
-import React, { useState } from "react";
-import SliderAndDisplay from "../components/SliderAndDisplay";
-import UpDownComponent from "../components/UpDownComponent";
+import React, { useEffect, useState } from "react";
 
 export default function Webtronome() {
   //Beat Per Minute
   const [bpm, setBpm] = useState(60);
 
+  const [message, setMessage] = useState("");
+  const onMessage = (event) => setMessage(event.data);
+
+  useEffect(() => {
+    const worker = new Worker("worker.js");
+    worker.addEventListener("message", onMessage);
+    return () => {
+      worker.removeEventListener("error", onMessage);
+      worker.terminate();
+    };
+  }, []);
+
   return (
     <div className="metronome">
-      <SliderAndDisplay bpm={bpm} setBpm={setBpm} />
-      <UpDownComponent setBpm={setBpm} />
+      {/* <SliderAndDisplay bpm={bpm} setBpm={setBpm} />
+      <UpDownComponent setBpm={setBpm} /> */}
+      {/* //<button onClick={runSort}></button> */}
     </div>
   );
 }
